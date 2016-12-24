929349cd6974b8c7507eb8ef50765645e53b3b02
huangyi <coodeer@163.com>
2016-12-25 01:41:06 +0800

    add rbac config

---
+++ config/rbac.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    'connection' => 'mysql',
+
+    'user' => 'App\User',
+
+];
