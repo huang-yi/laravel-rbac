9f215b5c83ea67687d22a91a7523ed7a414031b8
huangyi <coodeer@163.com>
2016-12-26 00:02:56 +0800

    exceptions

---
+++ src/Exceptions/PermissionNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\Rbac\Exceptions;
+
+use Exception;
+
+class PermissionNotFoundException extends Exception
+{
+
+}
